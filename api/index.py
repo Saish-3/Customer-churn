9a4b433c84e15425ebc085ed888587b263ffd1c9
Saish-3 <saishbhujbal03@gmail.com>
2026-04-24 11:05:31 +0530

    test handler

--- api/index.py
+++ api/index.py
@@ -1,22 +1,7 @@
 import json
-import pickle
-import os
-
-model_path = os.path.join(os.path.dirname(__file__), "../churn_model.pkl")
-model = pickle.load(open(model_path, "rb"))
 
 def handler(request):
-    if request.method == "GET":
-        return {
-            "statusCode": 200,
-            "body": json.dumps({"message": "API working"})
-        }
-
-    if request.method == "POST":
-        data = json.loads(request.body)
-        prediction = model.predict([data["input"]])
-
-        return {
-            "statusCode": 200,
-            "body": json.dumps({"prediction": prediction.tolist()})
-        }
+    return {
+        "statusCode": 200,
+        "body": json.dumps({"message": "API working"})
+    }
