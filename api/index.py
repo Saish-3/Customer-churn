c15e80c3e3a6f35c3dc044e073d2dd6f3385f0cf
Saish-3 <saishbhujbal03@gmail.com>
2026-04-24 10:55:45 +0530

    fix handler

--- api/index.py
+++ api/index.py
@@ -1,19 +1,22 @@
-from fastapi import FastAPI
+import json
 import pickle
 import os
 
-app = FastAPI()
-
-# Load model
 model_path = os.path.join(os.path.dirname(__file__), "../churn_model.pkl")
 model = pickle.load(open(model_path, "rb"))
 
-@app.get("/")
-def home():
-    return {"message": "Customer Churn API is running"}
+def handler(request):
+    if request.method == "GET":
+        return {
+            "statusCode": 200,
+            "body": json.dumps({"message": "API working"})
+        }
 
-@app.get("/predict")
-def predict():
-    data = [[0, 1, 2, 3, 4]]  # dummy input
-    result = model.predict(data)
-    return {"prediction": result.tolist()}
+    if request.method == "POST":
+        data = json.loads(request.body)
+        prediction = model.predict([data["input"]])
+
+        return {
+            "statusCode": 200,
+            "body": json.dumps({"prediction": prediction.tolist()})
+        }
